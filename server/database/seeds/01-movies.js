7bc8458ea6ff02def9f1560c3c1ed0b54c5d9411
Darragh Ferry <darraghferry@hotmail.com>
2023-02-25 00:12:56 +0000

    updated movies table

--- server/database/seeds/01-movies.js
+++ server/database/seeds/01-movies.js
@@ -7,19 +7,27 @@ let seed = (knex) => {
           name: "Spider-Man 2",
           rating: "12",
           director: "Sam Raimi",
-          cast: "Toby Maguie",
           runtime: 90,
           genre: "Action",
-          price: 8.5,
+          details: "Spiderman fights Doc Oc.",
+          trailer_url: "https://www.youtube.com/embed/1s9Yln0YwCw",
+          img_poster_url:
+            "https://m.media-amazon.com/images/M/MV5BMzY2ODk4NmUtOTVmNi00ZTdkLTlmOWYtMmE2OWVhNTU2OTVkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg",
+          img_landscape_url:
+            "https://ae01.alicdn.com/kf/HTB1y4LcIuuSBuNjSsplq6ze8pXaN/DIY-frame-The-Amazing-Spider-Man-2-Sci-fi-Movie-Film-Poster-Fabric-Silk-Urban-Landscape.jpg_Q90.jpg_.webp",
         },
         {
-          name: "Oppenheimer",
-          rating: "18",
+          name: "The Dark Knight",
+          rating: "12A",
           director: "Christopher Nolan",
-          cast: "Cillian Murphy",
-          runtime: 90,
-          genre: "Historic",
-          price: 8.5,
+          runtime: 152,
+          genre: "Action",
+          details: "Batman fights people.",
+          trailer_url: "https://www.youtube.com/embed/EXeTwQWrcwY",
+          img_poster_url:
+            "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg",
+          img_landscape_url:
+            "http://4.bp.blogspot.com/-tEYlVjT07Co/T85rVvpzF7I/AAAAAAAABTM/82ax2gx0RYw/w1200-h630-p-k-no-nu/dark-knight-rises-poster.jpg",
         },
       ]);
     });
