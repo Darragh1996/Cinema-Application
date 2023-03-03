2ad3bf855fb31742538cae8634392e1e7ece8c16
Joseph Foy <foyjoseph2104@gmail.com>
2023-03-03 11:09:45 +0000

    updated movie data

--- server/database/seeds/01-movies.js
+++ server/database/seeds/01-movies.js
@@ -17,17 +17,30 @@ let seed = (knex) => {
             "https://drive.google.com/uc?export=view&id=1LKaJoQLdjMQK528EYm0rCm7Xdm2o-CIM",
         },
         {
-          name: "The Dark Knight",
+          name: "Iron Man 2",
           rating: "12A",
-          director: "Christopher Nolan",
-          runtime: 152,
+          director: "Jon Favreau",
+          runtime: 126,
           genre: "Action",
-          details: "Batman fights people.",
-          trailer_url: "https://www.youtube.com/embed/EXeTwQWrcwY",
+          details: "Tony Stark becomes Iron man again",
+          trailer_url: "https://www.youtube.com/embed/qsRZghNciIo",
           img_poster_url:
             "https://drive.google.com/uc?export=view&id=1J_NooYoTQYY7wdSSNy5IEdTs2KVRsQBm",
           img_landscape_url:
             "https://drive.google.com/uc?export=view&id=12kI0Rg1N967jFVI0gsuv5wbUsT8Uj3An",
+        },
+        {
+          name: "Iron Man 3",
+          rating: "12A",
+          director: "Shane Black",
+          runtime: 130,
+          genre: "Action",
+          details: "Tony Stark takes on the Mandarin",
+          trailer_url: "https://www.youtube.com/embed/Ke1Y3P9D0Bc",
+          img_poster_url:
+            "https://drive.google.com/uc?export=view&id=1fcpIC-eUmzcgmqwiIoM3xwdyZhjDkCyF",
+          img_landscape_url:
+            "https://drive.google.com/uc?export=view&id=1HWc1hpVEm7QIbieypxiXBCNavQg1R9qG",
         },
       ]);
     });
