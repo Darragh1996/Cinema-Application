3971a4009213c72ffd9b37ea42b9e09645aa5c24
Darragh Ferry <darraghferry@hotmail.com>
2023-03-24 10:25:24 +0000

    a little code clean up

--- client/src/pages/bookSeats.js
+++ client/src/pages/bookSeats.js
@@ -7,44 +7,37 @@ import SeatPicker from "../components/seatPicker/seatPicker";
 function BookSeats() {
   const params = useParams();
   const [colCount, setColCount] = useState(0);
-  const [movieName, setMovieName] = useState('');
-  const [movieDescription, setMovieDescription] = useState('');
-  const [movieAgeRating, setMovieAgeRating] = useState('');
-  const [movieGenre, setMovieGenre] = useState('');
+  const [movieName, setMovieName] = useState("");
+  const [movieDescription, setMovieDescription] = useState("");
+  const [movieAgeRating, setMovieAgeRating] = useState("");
+  const [movieGenre, setMovieGenre] = useState("");
   const [selectedShowing, setSelectedShowing] = useState(params.showingID);
   const [dateOption, setDateOption] = useState([]);
-
 
   useEffect(() => {
     justAxios()
       .get(`/showings/${params.showingID}`)
       .then((res) => {
-        setMovieName(res.data.showing.name)
+        setMovieName(res.data.showing.name);
         justAxios()
           .get(`/screens/${res.data.showing.screenID}`)
           .then((res2) => {
             setColCount(res2.data.screen.colCount);
-            
           });
         justAxios()
           .get(`/movies/${res.data.showing.movieID}`)
-            .then((res3) => {
-              setMovieDescription(res3.data.movie.details);
-              setMovieAgeRating(res3.data.movie.rating);
-              setMovieGenre(res3.data.movie.genre)
-
-          })  
-          justAxios()
+          .then((res3) => {
+            setMovieDescription(res3.data.movie.details);
+            setMovieAgeRating(res3.data.movie.rating);
+            setMovieGenre(res3.data.movie.genre);
+          });
+        justAxios()
           .get("showings/view/" + res.data.showing.movieID)
           .then((res4) => {
-            console.log(res4)
-
             setDateOption(res4.data.showings);
           });
       });
-      
   }, []);
-  
 
   function formatDatetime(datetime) {
     const date = new Date(datetime);
@@ -66,50 +59,44 @@ function BookSeats() {
 
   return (
     <div>
-      <div class="container">
-
-        <div class="left-section">
-          <img src="" alt="" class="movie-img"/>
+      <div className="container">
+        <div className="left-section">
+          <img src="" alt="" className="movie-img" />
         </div>
-        <div class="right-section">
+        <div className="right-section">
+          <h2 className="movie-heading">{movieName}</h2>
 
-            <h2 class="movie-heading">{movieName}</h2>
+          <p className="sub-heading-actors">{movieGenre}</p>
 
-            <p class="sub-heading-actors">{movieGenre}</p>
+          <div className="movie-info">
+            <p>{movieAgeRating}</p>
+            <p>{movieDescription}</p>
+          </div>
 
-            <div class="movie-info">                
-                    <p>{movieAgeRating}</p>
-                    <p>{movieDescription}</p>
+          <div className="dropdown-section">
+            <div className="date">
+              <label htmlFor="date">showing</label>
+              <select
+                id="movieTimesBookingChoice"
+                name="movieTimesBookingChoice"
+                onChange={handleDateChange}
+                value={selectedShowing}
+              >
+                <option value="">Select Showing</option>
+                {dateOption.map((date) => (
+                  <option key={date.id} value={date.id}>
+                    {formatDatetime(date.datetime)}
+                  </option>
+                ))}
+              </select>
             </div>
-
-            <div class="dropdown-section">
-            
-            <div class="date">
-                <label for="date">showing</label>
-                <select 
-                  id="movieTimesBookingChoice"
-                  name="movieTimesBookingChoice"
-                  onChange={handleDateChange}
-                >
-                  <option value="">Select Showing</option>
-                  {dateOption.map((date) => (
-                    <option key={date.id} value={date.id} selected = {date.id === selectedShowing ? 'selected': 'false'}>
-                      {formatDatetime(date.datetime)}
-                    </option>
-                  ))}
-                </select>
-            </div>
-
-
-
-            </div>
+          </div>
         </div>
       </div>
       <div>
         <h1>Pick your seats.</h1>
         <SeatPicker showingID={params.showingID} colCount={colCount} />
       </div>
-      
     </div>
   );
 }
