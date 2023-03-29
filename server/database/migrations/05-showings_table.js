f9571189eebd6e5cd8706df5490cf696093b9b32
Darragh Ferry <darraghferry@hotmail.com>
2023-03-29 19:33:57 +0100

    added price and private columns to showings table

--- server/database/migrations/05-showings_table.js
+++ server/database/migrations/05-showings_table.js
@@ -16,6 +16,8 @@ let up = (knex) => {
       .inTable("screens")
       .onDelete("CASCADE");
     table.datetime("datetime").notNullable().defaultTo(knex.fn.now(6));
+    table.decimal("float").notNullable().defaultTo(8.0);
+    table.boolean("private").notNullable().defaultTo(false);
   });
 };
 
