a121f7d01c031c165cf440c61ba7c41747bfc9a5
unknown <l00154690@atu.ie>
2023-03-30 10:24:30 +0100

    NavBar

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,39 +1,106 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { justAxios } from "../../utils/axios";
 import { useNavigate } from "react-router-dom";
 
-
 import logo from "./img/logo.png";
-import userIcon from "./img/userIcon.png";
-
 import "../../styles.css";
 
 function NavBar() {
   const navigate = useNavigate();
-
+  const [isAccountOpen, setIsAccountOpen] = useState(false);
+  const dropdownRef = useRef(null);
 
   useEffect(() => {
-    
-  }, []);
-
-
-  let logout = () => {
-      sessionStorage.clear();                //Should clear data from user in that session
-      localStorage.removeItem('reel_dreams_jwt');    
-      navigate('/login');          
+    function handleClickOutside(event) {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target) &&
+        !event.target.closest("#account-button")
+      ) {
+        setIsAccountOpen(false);
+      }
     }
 
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [dropdownRef]);
+
+  const handleAccountClick = () => {
+    setIsAccountOpen((prevState) => !prevState);
+  };
+
+  const handleLogoutClick = () => {
+    sessionStorage.clear();
+    localStorage.removeItem("reel_dreams_jwt");
+    navigate("/login");
+  };
+
   return (
-    
-      <div id="nav">
-        <img src={logo} alt="Reel Dreams" />
-        <a href>
-          <h3>SHOWTIMES</h3>
-        </a>
-        <img id="userIcon" src={userIcon} alt="User Icon" />
-        <button onClick={logout}>Logout</button>
+    <div id="nav" style={{ display: "flex", alignItems: "center" }}>
+      <img src={logo} alt="Reel Dreams" style={{ width: "90px" }} />
+      <div style={{ flex: 1 }}></div>
+      <h1 style={{ textAlign: "left", margin: 0, flex: 2, color: "orange", paddingLeft: "40px" }}>Reel Dreams Cinema</h1>
+      <div
+        style={{
+          position: "relative",
+          marginLeft: "auto",
+        }}
+      >
+        <button
+          id="account-button"
+          style={{
+            backgroundColor: "#ffffff",
+            borderRadius: "50px",
+            border: "none",
+            padding: "10px 20px",
+            marginRight: "20px",
+            color: "#333",
+            fontWeight: "bold",
+            textTransform: "uppercase",
+            letterSpacing: "1px",
+            boxShadow: "0px 2px 5px rgba(0, 0, 0, 0.2)",
+            transition: "all 0.3s",
+          }}
+          onClick={handleAccountClick}
+        >
+          Account
+        </button>
+        {isAccountOpen && (
+          <div
+            ref={dropdownRef}
+            style={{
+              position: "absolute",
+              top: "calc(100% + 10px)",
+              right: 0,
+              backgroundColor: "#ffffff",
+              borderRadius: "10px",
+              padding: "10px",
+              boxShadow: "0px 8px 16px rgba(0, 0, 0, 0.1)",
+              zIndex: 1,
+              minWidth: "150px",
+              textAlign: "center",
+            }}
+          >
+            <button
+              onClick={handleLogoutClick}
+              style={{
+                backgroundColor: "orange",
+                color: "#ffffff",
+                borderRadius: "5px",
+                padding: "5px 10px",
+                border: "none",
+                cursor: "pointer",
+                transition: "all 0.3s",
+              }}
+            >
+              Log Out
+            </button>
+          </div>
+        )}
       </div>
-     
+    </div>
   );
 }
 
